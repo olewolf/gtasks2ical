3c1fac28296144aeef733ec8adc1d225d3db44ad
Ole Wolf <wolf@blazingangles.com>
2012-09-25 15:09:08 +0200

    Debian build dir added

---
+++ script/build-source-tarball.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make sure we're running the script from the proper directory.
+if [ ! -f AUTHORS ]; then
+    echo "Must be run from the package root"
+    exit 1
+fi
+
+# Clean up the directory.
+if [ -f Makefile ]; then
+    make distclean
+elif [ -f src/aws-s3fs ]; then
+    make clean
+fi
+find -name \*~ -delete
+
+# Create a tarball name based on the package directory.
+DIR=`basename $PWD`
+PACKAGE=`echo ${DIR} | sed -n "s/-\([0-9]\+\.[0-9]\+\)//p"`
+VERSION=`echo ${DIR} | sed -n "s/${PACKAGE}-\([0-9]\+\.[0-9]\+\)/\1/p"`
+cd ..
+tar -cjhf ${PACKAGE}_${VERSION}.orig.tar.xz --auto-compress --exclude=${DIR}/debian/* --exclude=${DIR}/.git/* --exclude=${DIR}/.gitignore ${DIR}
+
